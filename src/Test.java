979146b212576f68a18902898bb488f9d852877d
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-01-14 21:39:05 +0900

    Fix: indent setting

--- src/Test.java
+++ src/Test.java
@@ -8,14 +8,14 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		/*
-		HiTechnicIRSeekerV2 seek = new HiTechnicIRSeekerV2(SensorPort.S1);	//インスタンス作成
-	    float direction[] = new float[5];	//データ格納変数
-	    seek.fetchSample(direction,0);		//データ取得メソッド
-	    */
-		Ev3MotorSample motorSample = new Ev3MotorSample();
-		motorSample.execSample();
-	}
+    public static void main(String[] args) {
+	/*
+	HiTechnicIRSeekerV2 seek = new HiTechnicIRSeekerV2(SensorPort.S1);	//インスタンス作成
+	float direction[] = new float[5];	//データ格納変数
+	seek.fetchSample(direction,0);		//データ取得メソッド
+	 */
+	Ev3MotorSample motorSample = new Ev3MotorSample();
+	motorSample.execSample();
+    }
 
 }
