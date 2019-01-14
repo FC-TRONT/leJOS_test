6200b6b62a5124b31ac087ed2b37361a1e06af6a
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-01-14 21:40:37 +0900

    Fix: indent

--- src/Ev3MotorSample.java
+++ src/Ev3MotorSample.java
@@ -1,3 +1,4 @@
+
 /*!
  * @file Ev3MotorSample.java
  * @brief モータ制御サンプルプログラム
@@ -19,64 +20,64 @@ import lejos.hardware.lcd.TextLCD;
  * @brief モータの動作テストを行うクラス
  */
 public class Ev3MotorSample {
-	/*! 動作テストを実行する関数 */
-	public void execSample() {
-		//  必要インスタンス生成
-		RegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.B);
-		EV3 ev3 = (EV3) BrickFinder.getLocal();
-		TextLCD lcd = ev3.getTextLCD();
+    /* ! 動作テストを実行する関数 */
+    public void execSample() {
+	// 必要インスタンス生成
+	RegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.B);
+	EV3 ev3 = (EV3) BrickFinder.getLocal();
+	TextLCD lcd = ev3.getTextLCD();
 
-		//  初期画面表示
-		lcd.drawString("MotorSample", 1, 0);
+	// 初期画面表示
+	lcd.drawString("MotorSample", 1, 0);
 
-		while(true) {
-			//  速度10で前進
-			lcdShowAndForward(lcd, motor, 10);
-			Delay.msDelay(2000);
+	while (true) {
+	    // 速度10で前進
+	    lcdShowAndForward(lcd, motor, 10);
+	    Delay.msDelay(2000);
 
-			//  速度MAX/2で前進
-			lcdShowAndForward(lcd, motor, (int)(motor.getMaxSpeed()/2));
-			Delay.msDelay(2000);
+	    // 速度MAX/2で前進
+	    lcdShowAndForward(lcd, motor, (int) (motor.getMaxSpeed() / 2));
+	    Delay.msDelay(2000);
 
-			//  速度MAXで前進
-			lcdShowAndForward(lcd, motor, (int)motor.getMaxSpeed());
-			Delay.msDelay(2000);
+	    // 速度MAXで前進
+	    lcdShowAndForward(lcd, motor, (int) motor.getMaxSpeed());
+	    Delay.msDelay(2000);
 
-			//  速度10で後退
-			lcdShowAndBackward(lcd, motor, 10);
-			Delay.msDelay(2000);
+	    // 速度10で後退
+	    lcdShowAndBackward(lcd, motor, 10);
+	    Delay.msDelay(2000);
 
-			//  速度MAX/2で後退
-			lcdShowAndBackward(lcd, motor, (int)(motor.getMaxSpeed()/2));
-			Delay.msDelay(2000);
+	    // 速度MAX/2で後退
+	    lcdShowAndBackward(lcd, motor, (int) (motor.getMaxSpeed() / 2));
+	    Delay.msDelay(2000);
 
-			//  速度MAXで後退
-			lcdShowAndBackward(lcd, motor, (int)motor.getMaxSpeed());
-			Delay.msDelay(2000);
-		}
+	    // 速度MAXで後退
+	    lcdShowAndBackward(lcd, motor, (int) motor.getMaxSpeed());
+	    Delay.msDelay(2000);
 	}
+    }
 
-	/*! LCDに情報を表示し、前進する関数 */
-	private static void lcdShowAndForward(TextLCD lcd, RegulatedMotor motor, int speed) {
-		String lcdTemplate = "Speed = ";
-		//  LCD表示
-		lcd.drawString("Mode = Forward", 1, 1);
-		lcd.drawString(lcdTemplate + speed, 1, 2);
-		//  スピード設定
-		motor.setSpeed(speed);
-		//  前進
-		motor.forward();
-	}
+    /* ! LCDに情報を表示し、前進する関数 */
+    private static void lcdShowAndForward(TextLCD lcd, RegulatedMotor motor, int speed) {
+	String lcdTemplate = "Speed = ";
+	// LCD表示
+	lcd.drawString("Mode = Forward", 1, 1);
+	lcd.drawString(lcdTemplate + speed, 1, 2);
+	// スピード設定
+	motor.setSpeed(speed);
+	// 前進
+	motor.forward();
+    }
 
-	/*! LCDに情報を表示し、後退する関数 */
-	private static void lcdShowAndBackward(TextLCD lcd, RegulatedMotor motor, int speed) {
-		String lcdTemplate = "Speed = ";
-		//  LCD表示
-		lcd.drawString("Mode = Backward", 1, 1);
-		lcd.drawString(lcdTemplate + speed, 1, 2);
-		//  スピード設定
-		motor.setSpeed(speed);
-		//  前進
-		motor.backward();
-	}
+    /* ! LCDに情報を表示し、後退する関数 */
+    private static void lcdShowAndBackward(TextLCD lcd, RegulatedMotor motor, int speed) {
+	String lcdTemplate = "Speed = ";
+	// LCD表示
+	lcd.drawString("Mode = Backward", 1, 1);
+	lcd.drawString(lcdTemplate + speed, 1, 2);
+	// スピード設定
+	motor.setSpeed(speed);
+	// 前進
+	motor.backward();
+    }
 }
